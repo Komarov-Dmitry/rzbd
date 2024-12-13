9ca354dea9bf60ef95d01cea4d10e63c6cf54554
Komarov-Dmitry <komarov.mitia2020@gmail.com>
2024-12-13 20:10:01 +0300

    Initial Commit

---
+++ RZBD/RZBDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RZBDApp.swift
+//  RZBD
+//
+//  Created by Дмитрий Комаров on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RZBDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
